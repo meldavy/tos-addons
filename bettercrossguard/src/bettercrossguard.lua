8ed338aa4d85237968255f66ec6eff851ea4dddb
meldavy <jistyle.jty@gmail.com>
2021-09-23 05:28:34 -0700

    improve crossguard effect

--- bettercrossguard/src/bettercrossguard.lua
+++ bettercrossguard/src/bettercrossguard.lua
@@ -46,7 +46,7 @@ function BetterCrossguard.PlayEffectOnCrossguard(self, argNum)
         local targetHandle = session.GetTargetHandle()
         local target = world.GetActor(targetHandle)
         effect.PlayActorEffect(actor, 'F_warrior_shield002', 'None', 2.0, 15.0)
-        effect.PlayActorEffect(target, 'F_warrior_greatbearformation_cast_spin', 'None', 1.0, 3.0)
+        effect.PlayActorEffect(actor, "F_sys_expcard_great", 'None', 1.0, 4.0)
         effect.PlayActorEffect(target, 'F_warrior_greatbearformation_shot_ground', 'None', 1.0, 3.0)
     end
 end
