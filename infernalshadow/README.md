b68c16092eee28e5dbd3e61edc148789b1efb479
meldavy <jistyle.jty@gmail.com>
2021-09-22 20:58:01 -0700

    infernalshadow v1.0.0 release

--- infernalshadow/README.md
+++ infernalshadow/README.md
@@ -1,3 +1,43 @@
 # 인퍼널 섀도우
 인퍼널 섀도우 사용시 인퍼널 섀도우로 자동 타겟팅 해주는 애드온을 만들고 있습니다.
-타겟을 변경 하는 api가 없다보니 Ebisuke제작자분의 enhancedtargetlock이 사용하는 방법으로 일단 제작은 해봤는데, 실전에서는 생각보다 성공률이 좋지 않아서 소스만 올리고 나중에 결과가 좋아지면 ipf 배포 하겠슴...
+
+타겟을 변경 하는 api가 없다보니 Ebisuke제작자분의 enhancedtargetlock이 사용하는 방법으로 일단 제작은 해봤습니다.
+
+마우스 커서를 대상에게 이동을 하고 마우스모드로 잠시 전환 이후 다시 키보드모드로 전환 하는 방식인데요,
+* 키보드모드를 사용하고있어야함 (마우스모드 호환x)
+* 마우스모드로 잠깐씩 바뀌기때문에 키 바인딩 (이동키, 스킬키) 등등 키보드모드와 마우스모드를 동일하게 바꾸고 실행하는것을 추천
+* 무조건 타겟 락 (기본 capslock) 을 설정해야함
+
+사용방법:
+* 만약 현 타겟이 이미 인퍼널이라면 아무 동작도 없음
+* 만약 현 타겟이 인퍼널이 아니라면 인퍼널 섀도우 스킬 을 다시 누르면 주변 인퍼널 새도우 검색
+* 검색 성공하면 인퍼널 섀도우로 타겟 락 시도
+* 만약 주변에 인퍼널이 없고, 보스가 타겟이 아니라면 주변 보스 탐색 이후 보스에게 타겟 락 시도
+* `/infernal off` 혹은 `/인퍼널 off` 명령어로 보스 탐색 기능 끌수있음
+* off상태에서는 주변에 인퍼널이 있을시 인퍼널 타겟팅만 시도함
+* on 상태에서는 주변에 인퍼널이 없을시 보스 타겟팅까지 시도함
+
+간단요약: 인퍼널 스킬 마구 눌러
+
+-----------
+# Infernal Shadow
+This is an infernal shadow auto-target addon.
+
+Since there is no good API for target switching, I am using Ebisuke's enhancedtargetlock code and method.
+
+It utilizes temporarily swapping to mouse mode and attempting to move mouse around until a desired target is locked on,
+which user is switched back to keyboard mode.
+* Only works on keyboard mode
+* Requires target lock enabled (capslock by default)
+* Recommended that keybinds for skill and movement is same in mouse and keyboard mode
+
+How to use:
+* Press on infernal shadow's hotkey to trigger search
+* If target is not already infernal shadow, this will attempt to change target to nearest infernal shadow
+* If there is no nearby infernal shadow, then instead try to lock on to nearest boss
+* `/infernal off` can be used to disable nearby boss search
+* When in `off` state, addon only searches for nearby infernal shadow
+* When in `on` state, addon does double duty of also searching for nearby boss.
+
+Thus, by repeatedly pressing on infernal shadow hotkey, the user can easily swap to the shadow
+and swap back to the boss once the shadow is gone.
--- infernalshadow/src/infernalshadow.lua
+++ infernalshadow/src/infernalshadow.lua
@@ -1,29 +1,56 @@
 -- areas defined
 local author = 'meldavy'
 local addonName = 'infernalshadow'
+-- 가까운 보스 검색, 그리고 탐색 기능은 https://github.com/ebisuke/TosAddons/tree/master/enhancedtargetlock 에서 따옴
 _G['ADDONS'] = _G['ADDONS'] or {}
 _G['ADDONS'][author] = _G['ADDONS'][author] or {}
 _G['ADDONS'][author][addonName] = _G['ADDONS'][author][addonName] or {}
 
-CHAT_SYSTEM("/infernal 혹은 /인퍼널 명령어로 인퍼널 섀도우 애드온 설정 확인하세요!")
 -- get a pointer to the area
 local InfernalShadow = _G['ADDONS'][author][addonName]
 local acutil = require('acutil')
 
-local infernalshadowid = 21613
+InfernalShadow.SearchForBoss = false
+
+ui.SysMsg("인퍼널 애드온이 작동중입니다. /인퍼널 off 혹은 /infernal off 로 보스 탐색 기능을 해제 할수있습니다.")
+CHAT_SYSTEM("인퍼널 애드온이 작동중입니다. /인퍼널 off 혹은 /infernal off 로 보스 탐색 기능을 해제 할수있습니다.")
+
+local infernalshadowskillid = 21613
 local infernalshadowdebuffid = 2201
-local infernalshadowhandle = -1
+local searchhandle = nil
 local infernalshadowsearchlock = 0
 local testbox = {
     {0, 0, 0},
+    {-25, 0, 0},
+    {-12, 0, 0},
+    {12, 0, 0},
+    {25, 0, 0},
+
     {0, 5, 0},
+    {-25, 5, 0},
+    {-12, 5, 0},
+    {12, 5, 0},
+    {25, 5, 0},
+
     {0, 10, 0},
+    {-25, 10, 0},
+    {-12, 10, 0},
+    {12, 10, 0},
+    {25, 10, 0},
+
     {0, 15, 0},
+    {-25, 15, 0},
+    {-12, 15, 0},
+    {12, 15, 0},
+    {25, 15, 0},
+
     {0, 25, 0},
-    {0, 35, 0},
-    {0, 45, 0},
-    {0, 55, 0},
+    {-25, 25, 0},
+    {-12, 25, 0},
+    {12, 25, 0},
+    {25, 25, 0}
 }
+local searchattempts = 1
 local firstpos = nil
 function INFERNALSHADOW_ON_INIT(addon, frame)
     InfernalShadow.addon = addon;
@@ -34,12 +61,32 @@ function INFERNALSHADOW_ON_INIT(addon, frame)
     -- 타겟 변경
     addon:RegisterMsg('TARGET_SET', 'INFERNALSHADOW_ON_TARGET_SET');
     addon:RegisterMsg('TARGET_UPDATE', 'INFERNALSHADOW_ON_TARGET_UPDATE');
+    -- 명령어
+    acutil.slashCommand('/infernal', INFERNALSHADOW_PROCESS_COMMAND)
+    acutil.slashCommand('/인퍼널', INFERNALSHADOW_PROCESS_COMMAND)
+end
+
+function INFERNALSHADOW_PROCESS_COMMAND(command)
+    local cmd = '';
+    if #command > 0 then
+        cmd = table.remove(command, 1);
+    end
+    if cmd == 'on' then
+        InfernalShadow.SearchForBoss = true
+        ui.SysMsg("인퍼널섀도우 보스 탐색 기능 활성화됨")
+    elseif cmd == 'off' then
+        InfernalShadow.SearchForBoss = false
+        ui.SysMsg("인퍼널섀도우 보스 탐색 기능 비활성화됨")
+    else
+        ui.SysMsg("인퍼널 애드온이 작동중입니다. /인퍼널 off 혹은 /infernal off 로 보스 탐색 기능을 해제 할수있습니다.")
+    end
 end
 
 -- 인퍼널 섀도우로 타겟이 변경 완료 확인시 실행 멈춤
 function INFERNALSHADOW_ON_TARGET_SET(msgFrame, msg, argStr)
     local handle = session.GetTargetHandle();
-    if handle == infernalshadowhandle or infernalshadowhandle > 0 then
+    -- 타겟이 인퍼널 섀도우던가, 주변에 인퍼널 섀도우가 없을때
+    if handle == searchhandle or searchhandle == nil then
         END_INFERNAL_TARGET_SEARCH_ACTIVITY()
     end
 end
@@ -47,7 +94,7 @@ end
 -- 인퍼널 섀도우로 타겟이 변경 완료 확인시 실행 멈춤
 function INFERNALSHADOW_ON_TARGET_UPDATE(msgFrame, msg, argStr, argNum)
     local handle = session.GetTargetHandle();
-    if handle == infernalshadowhandle or infernalshadowhandle > 0 then
+    if handle == searchhandle or searchhandle == nil then
         END_INFERNAL_TARGET_SEARCH_ACTIVITY()
     end
 end
@@ -58,7 +105,7 @@ function INFERNALSHADOW_USE_SKILL(frame, slot, argStr, argNum)
     if icon ~= nil then
         local iconInfo = icon:GetInfo();
         if iconInfo:GetCategory() == 'Skill' then
-            if (iconInfo.type == infernalshadowid) then
+            if (iconInfo.type == infernalshadowskillid) then
                 -- 인퍼널 사용 확인 완료
                 InfernalShadow:ProcessInfernalShadow()
             end
@@ -67,47 +114,16 @@ function INFERNALSHADOW_USE_SKILL(frame, slot, argStr, argNum)
     QUICKSLOTNEXPBAR_SLOT_USE_OLD(frame, slot, argStr, argNum)
 end
 
-function InfernalShadow.ProcessInfernalShadow(self)
-    local list, count = SelectObject(GetMyPCObject(), 10000, 'ALL')
-    for i = 1, count do
-        local handle = GetHandle(list[i])
-        -- 주변 오브젝트가 인퍼널섀도우인지 확인
-        if (InfernalShadow:IsInfernalShadow(handle)) then
-            infernalshadowhandle = handle
-            -- 이미 타겟이 인퍼널 섀도우인지 확인
-            if (session.GetTargetHandle() ~= handle) then
-                firstpos = {mouse.GetX(), mouse.GetY()}
-                -- 인퍼널 타겟 찾기 작업이 진행중일땐 실행 하지 말것
-                if infernalshadowsearchlock == 0 then
-                    infernalshadowsearchlock = 1
-                    -- 찾기 과정 실행
-                    BEGIN_INFERNAL_TARGET_SEARCH_ACTIVITY()
-                else
-                    return
-                end
-            else
-                -- 만약 이미 타겟이라면 애드온 실행 취소
-                END_INFERNAL_TARGET_SEARCH_ACTIVITY()
-            end
-            return
-        end
-    end
-    -- 주변에 인퍼널 섀도우가 없다면 실행 취소
-    END_INFERNAL_TARGET_SEARCH_ACTIVITY()
-end
-
+-- 탐색 실행
 function BEGIN_INFERNAL_TARGET_SEARCH_ACTIVITY()
-    if (session.GetTargetHandle() == infernalshadowhandle or infernalshadowhandle < 0) then
+    if (session.GetTargetHandle() == searchhandle or searchhandle == nil) then
         -- short circuit
         END_INFERNAL_TARGET_SEARCH_ACTIVITY()
         return
     end
-    -- enhancedtargetlock 에서 따온 코드, 인퍼널 위치로 마우스 고정 후, 마우스모드로 전환
-    local handle = infernalshadowhandle
-    session.config.SetMouseMode(true)
-    mouse.SetHidable(0);
-    local cur = testbox[1]
-    local targetactor = world.GetActor(handle)
+    -- enhancedtargetlock 에서 따온 코드, 인퍼널 위치로 마우스 고정 시도
+    local cur = testbox[searchattempts]
+    local targetactor = world.GetActor(searchhandle)
     local pos = targetactor:GetPos()
     local pts = world.ToScreenPos(pos.x + cur[1], pos.y + cur[2], pos.z + cur[3]);
     local crx = pts.x
@@ -117,14 +133,17 @@ function BEGIN_INFERNAL_TARGET_SEARCH_ACTIVITY()
         crx = crx * 2
         cry = cry * 2
     end
-
-    --ensure
     if (crx >= 0 and cry >= 0 and crx < option.GetClientWidth() and cry < option.GetClientHeight()) then
         mouse.SetPos(crx, cry)
-        if infernalshadowsearchlock == 1 then
-            ReserveScript("BEGIN_INFERNAL_TARGET_SEARCH_ACTIVITY()", 0.01)
+    end
+    --ensure
+    if infernalshadowsearchlock == 1 then
+        searchattempts = searchattempts + 1
+        -- 횟수 초과시 실행 멈춤
+        if (searchattempts > #testbox) then
+            END_INFERNAL_TARGET_SEARCH_ACTIVITY()
         end
-    else
+        ReserveScript("BEGIN_INFERNAL_TARGET_SEARCH_ACTIVITY()", 0.005)
     end
 end
 
@@ -136,7 +155,91 @@ function END_INFERNAL_TARGET_SEARCH_ACTIVITY()
     end
     session.config.SetMouseMode(false)
     mouse.SetHidable(0);
-    infernalshadowhandle = -1
+    searchhandle = nil
+    searchattempts = 1
+end
+
+function InfernalShadow.ProcessInfernalShadow(self)
+    -- 인퍼널 섀도우 검색
+    local infernalhandle = InfernalShadow:FindNearbyInfernalShadow()
+    if (infernalhandle ~= nil) then
+        searchhandle = infernalhandle
+    else
+        -- 검색 실패시 가장 가까운 보스 검색
+        local bosshandle = InfernalShadow:FindNearbyBoss()
+        if (bosshandle ~= nil) then
+            searchhandle = bosshandle
+        end
+    end
+    -- 검색 성공시
+    if (searchhandle ~= nil) then
+        -- 현타겟이 탐색 대상이 아니면 실행
+        if (session.GetTargetHandle() ~= searchhandle) then
+            -- 타겟 탐색 작업이 이미 진행중일땐 중복 실행 방지
+            if infernalshadowsearchlock == 0 then
+                -- 타겟 탐색 시작
+                infernalshadowsearchlock = 1
+                firstpos = {mouse.GetX(), mouse.GetY()}
+                session.config.SetMouseMode(true)
+                mouse.SetHidable(0);
+                ReserveScript("BEGIN_INFERNAL_TARGET_SEARCH_ACTIVITY()", 0.01)
+            else
+                -- 이미 탐색 작업중일땐 실행 방지
+                return
+            end
+        else
+            -- 이미 타겟이 탐색 대상이라면 실행 중지
+            END_INFERNAL_TARGET_SEARCH_ACTIVITY()
+        end
+        return
+    else
+        -- 주변에 탐색 대상(인퍼널이나 보스)이 없다면 실행 중지
+        END_INFERNAL_TARGET_SEARCH_ACTIVITY()
+    end
+end
+
+function InfernalShadow.FindNearbyInfernalShadow(self)
+    local list, count = SelectObject(GetMyPCObject(), 500, 'ALL')
+    for i = 1, count do
+        local handle = GetHandle(list[i])
+        -- 주변 오브젝트가 인퍼널섀도우인지 확인
+        if (InfernalShadow:IsInfernalShadow(handle)) then
+            return handle
+        end
+    end
+    return nil
+end
+
+function InfernalShadow.FindNearbyBoss(self)
+    local nearestdistance = 999999999
+    local mypos = GetMyActor():GetPos()
+    local nearestEnemy = nil
+    local objList, objCount = SelectObject(GetMyActor(), 500, 'ENEMY')
+
+    --ボスを検索
+    for i = 1, objCount do
+        local enemyHandle = GetHandle(objList[i])
+        local enemyActor = world.GetActor(enemyHandle)
+        local monsterClass = GetClassByType("Monster", enemyActor:GetType());
+
+        if (monsterClass ~= nil) then
+            if (monsterClass.MonRank == "Boss") then
+                --死んでたら除外
+                local stat = info.GetStat(enemyHandle)
+                if (stat.HP > 0) then
+                    local enemypos = enemyActor:GetPos()
+                    --距離を測る
+                    local dist = math.sqrt((mypos.x - enemypos.x) ^ 2 + (mypos.z - enemypos.z) ^ 2)
+                    if (dist < nearestdistance) then
+                        nearestEnemy = enemyHandle
+                        nearestdistance = dist
+                    end
+
+                end
+            end
+        end
+    end
+    return nearestEnemy
 end
 
 function InfernalShadow.IsInfernalShadow(self, handle)
